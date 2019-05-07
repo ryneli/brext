f1c090052d39fc9be2a73e59a6ca664944b8a4fb
Zhenqiang Li <lizhenqiangsnake@gmail.cm>
2019-05-06 21:26:00 -0400

    Updates

---
+++ precache-manifest.918d8c05c10d05d6ad0a663dcb1a2b2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979270ff34e750af4b77533f9cab5551",
+    "url": "/brext/index.html"
+  },
+  {
+    "revision": "b96199a33fb3f1dc31d5",
+    "url": "/brext/static/css/main.64908277.chunk.css"
+  },
+  {
+    "revision": "d00d1e8877f2646063f2",
+    "url": "/brext/static/js/2.7e44223c.chunk.js"
+  },
+  {
+    "revision": "b96199a33fb3f1dc31d5",
+    "url": "/brext/static/js/main.aa942b90.chunk.js"
+  },
+  {
+    "revision": "b9f46a6c533bd06a40a9",
+    "url": "/brext/static/js/runtime~main.eac97156.js"
+  }
+]);
